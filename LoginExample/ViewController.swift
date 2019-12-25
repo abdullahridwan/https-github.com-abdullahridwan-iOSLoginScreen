373c4b4ba4f55d9e9ce28038aa9f4ca27c73de2a
Abdullah Ridwan <53100755+abdullahridwan@users.noreply.github.com>
2019-12-25 10:53:41 -0500

    Initial Commit

---
+++ LoginExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginExample
+//
+//  Created by Abdullah Ridwan on 12/25/19.
+//  Copyright © 2019 Abdullah Ridwan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
